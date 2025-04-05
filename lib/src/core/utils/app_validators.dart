0418fcd590a14b01677dde8f3bbc478d9b91dc71
juniRanger <raincolor54@gmail.com>
2025-04-05 01:14:45 -0600

    proyecto terminado

--- lib/src/core/utils/app_validators.dart
+++ lib/src/core/utils/app_validators.dart
@@ -1,7 +1,4 @@
 class AppValidators {
-  // static bool isValidEmail(String email) => RegExp(
-  //       "^[a-zA-Z0-9.!#\$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*\$",
-  //     ).hasMatch(email);
 
   static bool isValidPassword(String password) => password.length > 7;
 
